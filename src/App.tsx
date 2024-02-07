af9bc3a4bc6adae9dd7b392ecaea9c2bb9e47c58
congbinh75 <dangcongbinh36@gmail.com>
2024-02-07 10:58:37 +0700

    Add Sidebar

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Sidebar from './components/sidebar/Sidebar'
+
+function App() {
+  return (
+    <div className='min-h-screen bg-gray-100'>
+      <Sidebar />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'    
+
+function Sidebar() {
+    
+    
+    return (
+        <div className='grid grid-cols-4 gap-4 h-full bg-black'>
+
+        </div>
+    )
+}
+
+export default Sidebar
