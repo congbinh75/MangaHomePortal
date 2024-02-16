02df8cbd427bfb1abd6190260cee147e7483cbda
congbinh75 <dangcongbinh36@gmail.com>
2024-02-16 15:56:43 +0700

    Modify Sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react"
-import SidebarHeader from "./SidebarHeader"
 import SidebarOption from "./SidebarOption";
 
 const Sidebar = () =>  {
@@ -13,21 +12,27 @@ const Sidebar = () =>  {
 
         switch(name){
             case 'Updates': {
+                setCurrentOption('Updates');
                 break;
             }
-            case 'Your lists': {
+            case 'Your Lists': {
+                setCurrentOption('Your Lists');
                 break;
             }
             case 'History': {
+                setCurrentOption('History');
                 break;
             }
-            case 'Advanced search': {
+            case 'Advanced Search': {
+                setCurrentOption('Advanced Search');
                 break;
             }
             case 'Random': {
+                setCurrentOption('Random');
                 break;
             }
-            case 'About us': {
+            case 'About Us': {
+                setCurrentOption('About Us');
                 break;
             }
             default: {
@@ -39,21 +44,50 @@ const Sidebar = () =>  {
     }
 
     return (
-        <div className="w-64 min-h-screen p-10 bg-gray-200">
-            <div className="sidebar-group">
-                <SidebarHeader name='Library' icon="bookmark" />
-                <SidebarOption name='Updates' func={onOptionChosen} />
-                <SidebarOption name='Your lists' func={onOptionChosen} />
-                <SidebarOption name='History' func={onOptionChosen} />
+        <div className="w-56 p-5 min-h-screen bg-slate-200 dark:bg-slate-900 shadow overflow-y-auto max-h-screen">
+            <div className="w-full flex flex-row-reverse">
+                <button>
+                    <i className="fa-solid fa-x" />
+                </button>
             </div>
-            <div className="sidebar-group">
-                <SidebarHeader name='Titles' icon="book" />
-                <SidebarOption name='Advanced search' func={onOptionChosen} />
-                <SidebarOption name='Random' func={onOptionChosen} />
+            <div className="mb-2 p-3 pl-1 w-full">
+                <img src="/src/assets/img/logo_darkbg.png"/>
             </div>
-            <div className="sidebar-group">
-                <SidebarHeader name='MangaHome' icon="at" />
-                <SidebarOption name='About us' func={onOptionChosen} />
+            <div className="mb-3 pb-2 w-full">
+                <div className="font-bold">
+                    Library
+                </div>
+                <div className="shadow-y w-full">
+                    <SidebarOption name='Updates' func={onOptionChosen} icon="bookmark" currentOption={currentOption} />
+                    <SidebarOption name='Your Lists' func={onOptionChosen} icon="book" currentOption={currentOption} />
+                    <SidebarOption name='History' func={onOptionChosen} icon="clock-rotate-left" currentOption={currentOption} />
+                </div>
+            </div>
+            <div className="mb-3 pb-2">
+                <div className="font-bold">
+                    Titles
+                </div>  
+                <div className="shadow-y w-full">
+                    <SidebarOption name='Advanced Search' func={onOptionChosen} icon="magnifying-glass" currentOption={currentOption} />
+                    <SidebarOption name='Random' func={onOptionChosen} icon="dice" currentOption={currentOption} />
+                </div>
+            </div>
+            <div className="mb-3 pb-2 w-full">
+                <div className="font-bold">
+                    Community
+                </div>
+                <div className="shadow-y w-full">
+                    <SidebarOption name='Users' func={onOptionChosen} icon="user" currentOption={currentOption} />
+                    <SidebarOption name='Groups' func={onOptionChosen} icon="user-group" currentOption={currentOption} />
+                </div>
+            </div>
+            <div className="mb-3 pb-2">
+                <div className="font-bold">
+                    MangaHome
+                </div>
+                <div className="shadow-y w-full">
+                    <SidebarOption name='About Us' func={onOptionChosen} icon="at" currentOption={currentOption} />
+                </div>
             </div>
         </div>
     )
--- src/components/sidebar/SidebarOption.tsx
+++ src/components/sidebar/SidebarOption.tsx
@@ -1,13 +1,37 @@
 interface Props {
     name: string;
+    icon?: string;
     func: Function;
+    currentOption: string;
+}
+
+function getParentDivClassName(name: string, currentOption: string) {
+    let divClassName = "p-0.5 my-1 rounded"
+    if (currentOption === name) {
+        divClassName += " bg-teal-600"
+    }
+    else {
+        divClassName += " bg-slate-200 dark:bg-slate-900 hover:bg-teal-500"
+    }
+    return divClassName
+}
+
+function getIconClassName(icon?: string) {
+    let iconClassName = "ml-1 mr-2"
+    if (icon) {
+        iconClassName += " fa-solid w-5 fa-" + icon
+    }
+    return iconClassName
 }
 
 function SidebarOption(props: Props) {
     return (
-        <button className="sidebar-option" onClick={() => props.func(props.name)}>
-            {props.name}
-        </button>
+        <div className={getParentDivClassName(props.name, props.currentOption)}>
+            <button onClick={() => props.func(props.name)}>
+                <i className={getIconClassName(props.icon)} />
+                {props.name}
+            </button>
+        </div>
     )
 }
 
