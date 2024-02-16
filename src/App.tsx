394f908a3cce4418c3e0f2db0ed50c9a47c3c232
congbinh75 <dangcongbinh36@gmail.com>
2024-02-16 15:56:54 +0700

    Add Carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
+import Carousel from './components/carousel/Carousel'
 import Sidebar from './components/sidebar/Sidebar'
 
 function App() {
   return (
-    <div className='min-h-screen bg-gray-100'>
-      <Sidebar />
+    <div className='min-h-screen flex flex-row bg-slate-50 text-black dark:bg-slate-950 dark:text-white'>
+      <div className='flex-none'>
+        <Sidebar />
+      </div>
+      <div className='flex-1'>
+        <Carousel />
+      </div>
     </div>
   )
 }
